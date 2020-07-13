fb4ce04d6b57fd08a4da4a5644fdd24c52bb340c
Vivian Nguyen <vivian.nguyen1127@gmail.com>
2020-07-12 17:51:45 -0700

    Added regex filter for results

--- src/Main.java
+++ src/Main.java
@@ -6,19 +6,36 @@ import net.sourceforge.tess4j.TesseractException;
 public class Main {
 	public static void main(String[] args) 
     { 
-		 Tesseract tesseract = new Tesseract(); 
-	        try { 
-	  
-	            tesseract.setDatapath("/Users/viviannguyen/eclipse-workspace/ParkBot/src/Tess4J/tessdata/"); 
-	        	File imageFile = new File("/Users/viviannguyen/eclipse-workspace/ParkBot/src/Tess4J/tessdata/1general.png");
-
-	        	String text 
-	                = tesseract.doOCR(imageFile); 
-	  
-	            System.out.print(text); 
-	        } 
-	        catch (TesseractException e) { 
-	            e.printStackTrace(); 
-	        } 
+		 
+		//clock have picture taken every something seconds
+		//increment file name by 1
+		//sleep for x seconds
+		Tesseract tesseract = new Tesseract(); 
+		tesseract.setDatapath("/Users/viviannguyen/eclipse-workspace/ParkBot/src/Tess4J/tessdata/"); 
+		//filter out letters that are not 
+		File image = new File("/Users/viviannguyen/eclipse-workspace/ParkBot/src/Tess4J/tessdata/1handicap1ar.png");
+		process_image(tesseract, image);
+		        	
+		  
+	       
+	        
     }
+	
+	private static void process_image(Tesseract tesseract, File image) {
+        try { 
+        	String text = tesseract.doOCR(image); 
+            System.out.print(text); 
+            filter_results(text);
+
+        }
+    	catch (TesseractException e) { 
+            e.printStackTrace(); 
+        } 
+		//addto db
+	}
+	
+	private static void filter_results(String text) {
+		text = text.replaceAll("[^A-GKM-Z]+", "");
+		System.out.println(text);
+	}
 }
