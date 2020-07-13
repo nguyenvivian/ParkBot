4290f9ae118e2b2212db1510226507ee6bccd210
Vivian Nguyen <vivian.nguyen1127@gmail.com>
2020-07-12 22:14:37 -0700

    Added MySQL connection

--- src/Main.java
+++ src/Main.java
@@ -2,11 +2,20 @@ import java.io.File;
   
 import net.sourceforge.tess4j.Tesseract; 
 import net.sourceforge.tess4j.TesseractException; 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class Main {
 	public static void main(String[] args) 
     { 
-		 
+	    try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			Connection connection = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/sys?serverTimezone=US/Pacific", "root", "sql99server");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	    
 		//clock have picture taken every something seconds
 		//increment file name by 1
 		//sleep for x seconds
@@ -16,9 +25,6 @@ public class Main {
 		File image = new File("/Users/viviannguyen/eclipse-workspace/ParkBot/src/Tess4J/tessdata/1handicap1ar.png");
 		process_image(tesseract, image);
 		        	
-		  
-	       
-	        
     }
 	
 	private static void process_image(Tesseract tesseract, File image) {
@@ -27,8 +33,6 @@ public class Main {
             text = filter_results(text);
             System.out.print(text); 
            
-
-
         }
     	catch (TesseractException e) { 
             e.printStackTrace(); 
@@ -39,4 +43,7 @@ public class Main {
 	private static String filter_results(String text) {
 		return text.replaceAll("[^A-GKM-Z]+", "");
 	}
+	
+	
+	
 }
